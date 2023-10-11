51a5600db33c2510fced11242f7a6f70b540ec94
Pankhil07 <pankhilgawade2001@gmail.com>
2023-10-11 12:07:49 +0200

    init

--- src/models/tests/test_data.py
+++ src/models/tests/test_data.py
@@ -3,22 +3,18 @@ from transformers import AutoTokenizer
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
 #from sklearn.model_selection import train_test_split
-from  hydra.experimental import initialize, compose
+#from  hydra.experimental import initialize, compose
 import src
 from src import data
-import hydra
+#import hydra
 from src.data.data import split_data
 from omegaconf import DictConfig, OmegaConf
 import yaml
 
 # Load configuration from the YAML file
-with initialize(config_path="../../."):
-    cfg = compose(config_name="config.yml")
-
-# Access configuration parameters using OmegaConf
-config = OmegaConf.to_container(cfg)
-print("Configuration:")
-print(yaml.dump(config))
+config_path = os.path.join('..\\.', "config.yml")
+with open(config_path, "r") as config_file:
+    config = yaml.safe_load(config_file)
 # data_processing_config = cfg.data_processing
 # paths_config = cfg.paths
 def test_load_data():
@@ -31,8 +27,8 @@ def test_split_dataset():
     # data_processing_config = cfg.data_processing
     # paths_config = cfg.paths
 
-    raw_dataset = load_dataset(cfg.dataset_name, f"{cfg.source_language}-{cfg.target_language}")
-    train_set, validation_set, test_set = split_data(raw_dataset,cfg)
+    raw_dataset = load_dataset("opus_books", "en-it")
+    train_set, validation_set, test_set = split_data(raw_dataset,config)
 
     assert len(train_set) > 0  # Check if the training set is not empty
     assert len(validation_set) > 0  # Check if the validation set is not empty
